c985b251d1ce3cbed2e1b6c760adceb5351bf0dc
Bernardo Montoya <ber.montoya12@gmail.com>
2021-07-19 22:03:14 -0600

    feat: add spanish locale

---
+++ src/locales/es.ts
@@ -0,0 +1,29 @@
+const es = {
+  home: {
+    hero: {
+      title: "Ayudanos a encontrar a nuestros hermanos.",
+      reportButton: "Reportar Desaparecido",
+    },
+    stats: {
+      dissapeared: "Desaparecidos",
+      released: "Liberados",
+      toVerify: "Por verificar",
+    },
+    search: {
+      searchTitle: "Lorem ipsum dolor sit amet consectutar domor",
+      searchIndication:
+        "Escribe el nombre de la persona que estas tratando de localizar.",
+      searchPlaceholder: "John Doe",
+      searchButton: "Buscar",
+    },
+    pagination: {
+      showMoreButton: "Mostrar Más",
+    },
+    footer: {
+      email: "info@email.com",
+      contactUsButton: "Contactanos",
+    },
+  },
+};
+
+export default es;
