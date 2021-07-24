e61c574110ef23e3f3912b5108fba214b46da5a1
YOUR NAME <YOUR@EMAIL.com>
2021-07-24 17:02:32 -0400

    feat: adjusted grammar

--- src/pages/landing/index.tsx
+++ src/pages/landing/index.tsx
@@ -79,7 +79,7 @@ export default function Landing() {
       <Head>
         <title>Cubamissing.com | Ayudanos a encontrar a nuestros hermanos</title>
         <meta name="description" content="Un grupo de voluntarios que trabajan para apoyar al pueblo de Cuba, incluyendo a líderes de Raíces deEsperanza y Cubalex, anuncian el lanzamiento de https://cubamissing.com. Desaparecidos Cubanos esuna plataforma para documentar los arrestos, desapariciones y violaciones de derechos por parte delrégimen cubano hacia los ciudadanos después de las protestas nacionales que comenzaron el 11 de Julio." />
-        <meta property="og:title" content="Cubamissing.com | Ayúdanos a encontrar a nuestros hermanos" key="title" />
+        <meta property="og:title" content="Cubamissing.com | Ayúdanos a encontrar a nuestros hermanos." key="title" />
         <meta property="og:description" content="Un grupo de voluntarios que trabajan para apoyar al pueblo de Cuba, incluyendo a líderes de Raíces deEsperanza y Cubalex, anuncian el lanzamiento de https://cubamissing.com. Desaparecidos Cubanos esuna plataforma para documentar los arrestos, desapariciones y violaciones de derechos por parte delrégimen cubano hacia los ciudadanos después de las protestas nacionales que comenzaron el 11 de Julio." key="description" />
         <meta property="og:image" content="/facebook-image.jpg" />
         <meta property="og:image:width" content="1200" />
