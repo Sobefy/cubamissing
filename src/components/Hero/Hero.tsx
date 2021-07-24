365fb783d125a80c46bdfb175ddd9fdcb35e597b
Bernardo Montoya <ber.montoya12@gmail.com>
2021-07-24 15:56:04 -0600

    feat: change image url

--- src/components/Hero/Hero.tsx
+++ src/components/Hero/Hero.tsx
@@ -11,16 +11,33 @@ interface HeroProps {
 
 const Hero = ({ translations }: HeroProps) => {
   const { title, reportButton } = translations;
-  const myLoader = (params:any) => {
-    return `https://cubamissing.com/_next/image?url=%2Fhero-image.svg&w=3840&q=100`
-  }
+  const myLoader = (params: any) => {
+    return `https://cubamissing.com/_next/image?url=%2Fhero-image.svg&w=3840&q=100`;
+  };
   return (
-    <Box style={{backgroundImage:"url('https://cubamissing-media.s3.amazonaws.com/hero-image.jpeg')",backgroundRepeat:"no-repeat",backgroundSize:"cover"}} textAlign="center" background="white" py={20} position="relative">
-      
+    <Box
+      style={{
+        backgroundImage:
+          "url('https://cubamissing-media.s3.amazonaws.com/hero-image.jpeg')",
+        backgroundRepeat: "no-repeat",
+        backgroundSize: "cover",
+      }}
+      textAlign="center"
+      background="white"
+      py={20}
+      position="relative"
+    >
       <Container>
-        <Box pt={40} pb={40} style={{backgroundImage:"url('https://cubamissing-media.s3.amazonaws.com/hero-image.svg')",backgroundPosition:"center",backgroundRepeat:"no-repeat"}}>
-          
-        </Box>
+        <Box
+          pt={40}
+          pb={40}
+          style={{
+            backgroundImage:
+              "url('https://cubamissing-media.s3.amazonaws.com/hero-image.png')",
+            backgroundPosition: "center",
+            backgroundRepeat: "no-repeat",
+          }}
+        ></Box>
       </Container>
     </Box>
   );
