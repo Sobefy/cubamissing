2081bcdda8fa796af590f4aea7af315f66ba0ab6
YOUR NAME <YOUR@EMAIL.com>
2021-07-24 15:37:47 -0400

    feat: replaced logo

--- src/components/Hero/Hero.tsx
+++ src/components/Hero/Hero.tsx
@@ -24,7 +24,7 @@ const Hero = ({ translations }: HeroProps) => {
         <Box pt={40} pb={40}>
           <Image
             alt="Cuba"
-            src="/hero-image.svg"
+            src="/logo-cuba-missing.png"
             layout="fill"
             objectFit="contain"
             quality={100}
