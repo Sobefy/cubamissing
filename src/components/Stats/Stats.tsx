5c476f5c93a6704491f536ab75ef24b9dd52844d
YOUR NAME <YOUR@EMAIL.com>
2021-07-27 10:15:29 -0400

    feat: updated stats

--- src/components/Stats/Stats.tsx
+++ src/components/Stats/Stats.tsx
@@ -36,11 +36,11 @@ const Stats = ({ translations }: StatsProps) => {
           rowGap={{ base: 6, md: 0 }}
           textAlign="center"
         >
-          <StatCard label={dissapeared} number={38} gridColumn="1/3" />
-          <StatCard label={released} number={156} gridColumn="3/5" />
-          <StatCard label={detention} number={262} gridColumn="1/3" />
-          <StatCard label={toVerify} number={235} gridColumn="3/5" />
-          <StatCard label={total} number={691} gridColumn="1/5" />
+          <StatCard label={dissapeared} number={37} gridColumn="1/3" />
+          <StatCard label={released} number={163} gridColumn="3/5" />
+          <StatCard label={detention} number={266} gridColumn="1/3" />
+          <StatCard label={toVerify} number={238} gridColumn="3/5" />
+          <StatCard label={total} number={704} gridColumn="1/5" />
         </Grid>
       </Container>
     </Box>
