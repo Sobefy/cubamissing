66ace158e4235b543addd4f12a27d587f2f8936b
Bernardo Montoya <ber.montoya12@gmail.com>
2021-08-02 18:58:52 -0600

    feat: integrate google analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,27 @@
+import { useEffect } from "react";
+import { useRouter } from "next/router";
 import type { AppProps } from "next/app";
 import { ChakraProvider } from "@chakra-ui/react";
+
 import theme from "../src/styles/theme";
+import * as ga from "../lib/ga";
 
-import "@fontsource/poppins"
-import "@fontsource/karla"
-
+import "@fontsource/poppins";
+import "@fontsource/karla";
 
 function MyApp({ Component, pageProps }: AppProps) {
+  const router = useRouter();
+
+  useEffect(() => {
+    const handleRouteChange = (url: string) => {
+      ga.pageview(url);
+    };
+    router.events.on("routeChangeComplete", handleRouteChange);
+    return () => {
+      router.events.off("routeChangeComplete", handleRouteChange);
+    };
+  }, [router.events]);
+
   return (
     <ChakraProvider theme={theme}>
       <Component {...pageProps} />
